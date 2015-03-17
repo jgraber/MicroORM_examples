9f6912dec2d4935e085982b628b5fa19026f9d9a
Johnny Graber <jg@jgraber.ch>
2015-03-18 00:09:40 +0100

    add example to read from database

---
+++ MicroORM_Dapper/MicroORM_Dapper/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroORM_Dapper
+{
+    public class Book
+    {
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public int Pages { get; set; }
+        public string Summary { get; set; }
+        public decimal Rating { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("[{0}] {1} - {2}, {3}",
+                Id, Title.Substring(0, 30) , ISBN, Pages);
+        }
+    }
+}
--- MicroORM_Dapper/MicroORM_Dapper/Program.cs
+++ MicroORM_Dapper/MicroORM_Dapper/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using Dapper;
 
 namespace MicroORM_Dapper
 {
@@ -10,6 +13,27 @@ namespace MicroORM_Dapper
     {
         static void Main(string[] args)
         {
+            ReadData();
+        }
+
+        private static void ReadData()
+        {
+            using (var connection = Program.GetOpenConnection())
+            {
+                var books = connection.Query<Book>("SELECT * FROM Book").ToList();
+                foreach (var currentBook in books)
+                {
+                    Console.WriteLine(currentBook);
+                }
+            }
+        }
+
+        private static SqlConnection GetOpenConnection()
+        {
+            var cs = ConfigurationManager.ConnectionStrings["OrmConnection"].ConnectionString;
+            var connection = new SqlConnection(cs);
+            connection.Open();
+            return connection;
         }
     }
 }
