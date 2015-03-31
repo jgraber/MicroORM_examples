a106569171b0b43468cbb7e04690abb09a9b5747
Johnny Graber <jg@jgraber.ch>
2015-03-31 21:04:57 +0200

    add Massive

--- MicroORM_Dapper/MicroORM_Massive/Program.cs
+++ MicroORM_Dapper/MicroORM_Massive/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicroORM_Massive.Massive;
 
 namespace MicroORM_Massive
 {
@@ -10,7 +11,18 @@ namespace MicroORM_Massive
     {
         static void Main(string[] args)
         {
-
+            dynamic books = new Book().All();
+            foreach (var book in books)
+            {
+                Console.WriteLine(String.Format("[{0}] {1} - {2}, {3}",
+                    book.Id, new string(((string) book.Title).Take(30).ToArray()), book.ISBN, book.Pages));
+            }
         }
+    }
+
+    public class Book : DynamicModel
+    {
+        //you don't have to specify the connection - Massive will use the first one it finds in your config
+        public Book() : base("OrmConnection", "Book", "Id") { }
     }
 }
