7bc564c5d82bc7d08274a7cd8375891326d080f2
Johnny Graber <jg@jgraber.ch>
2015-04-07 21:07:48 +0200

    add example to read from the database (PetaPoco)

---
+++ MicroORM_Dapper/MicroORM_PetaPoco/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.IO;
+
+namespace MicroORM_Dapper
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public int Pages { get; set; }
+        public string Summary { get; set; }
+        public decimal Rating { get; set; }
+        
+        public override string ToString()
+        {
+            return String.Format("[{0}] {1} - {2}, {3}",
+                Id, new string(Title.Take(30).ToArray()), ISBN, Pages);
+        }
+    }
+}
--- MicroORM_Dapper/MicroORM_PetaPoco/Program.cs
+++ MicroORM_Dapper/MicroORM_PetaPoco/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicroORM_Dapper;
+using PetaPoco;
 
 namespace MicroORM_PetaPoco
 {
@@ -10,6 +12,16 @@ namespace MicroORM_PetaPoco
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Get all books");
+            var database = new Database("OrmConnection");
+
+            var books = database.Query<Book>("SELECT * FROM BOOK;");
+
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+
         }
     }
 }
