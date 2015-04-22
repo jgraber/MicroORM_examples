2a27ef00ef9c54ec86e2a316593f2cf7bbe4c964
Johnny Graber <jg@jgraber.ch>
2015-04-22 21:31:35 +0200

    add create script for quote and person tables

---
+++ MicroORM_Dapper/Quotations/SQL_Scripts/create_tables.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE Person
+(
+	Id		INT IDENTITY(1,1),
+	LastName	VARCHAR(100),
+	FirstName	VARCHAR(100),
+	Born		Date,
+	Died		Date,
+	CONSTRAINT pk_Person_Id PRIMARY KEY (Id)
+);
+Go
+
+CREATE TABLE Quote
+(
+	Id			INT IDENTITY(1,1),
+	Text		VARCHAR(6000),
+	Year		VARCHAR(4),
+	Context		VARCHAR(6000),
+	PersonId	Int,
+	CONSTRAINT pk_Quote_Id PRIMARY KEY (Id),
+	CONSTRAINT fk_Quote_Person FOREIGN KEY (PersonId) REFERENCES Person
+);
+
+GO
