ebc93c57f5e8b1b94196e73f99aa1dad9565427a
Johnny Graber <jg@jgraber.ch>
2015-05-23 13:10:59 +0200

    experiment with jquery.validate.globalize.js

--- MicroORM_Dapper/Quotations/App_Start/BundleConfig.cs
+++ MicroORM_Dapper/Quotations/App_Start/BundleConfig.cs
@@ -9,10 +9,11 @@ namespace Quotations
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.js", "~/Scripts/jquery.validate.globalize.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
---
+++ MicroORM_Dapper/Quotations/Helper/GlobalizeUrls.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Quotations.Helper
+{
+    public static class GlobalizeUrls
+    {
+        /// <summary>
+        /// URL for Globalize: https://github.com/jquery/globalize
+        /// </summary>
+        public static string Globalize { get { return "~/Scripts/globalize.js"; } }
+
+        /// <summary>
+        /// URL for the specific Globalize culture
+        /// </summary>
+        public static string GlobalizeCulture
+        {
+            get
+            {
+                //Determine culture - GUI culture for preference, user selected culture as fallback
+                var currentCulture = CultureInfo.CurrentCulture;
+                var filePattern = "~/Scripts/globalize/globalize.culture.{0}.js";
+                var regionalisedFileToUse = string.Format(filePattern, "en-GB"); //Default localisation to use
+
+                //Try to pick a more appropriate regionalisation
+                if (File.Exists(HostingEnvironment.MapPath(string.Format(filePattern, currentCulture.Name)))) //First try for a globalize.culture.en-GB.js style file
+                    regionalisedFileToUse = string.Format(filePattern, currentCulture.Name);
+                else if (File.Exists(HostingEnvironment.MapPath(string.Format(filePattern, currentCulture.TwoLetterISOLanguageName)))) //That failed; now try for a globalize.culture.en.js style file
+                    regionalisedFileToUse = string.Format(filePattern, currentCulture.TwoLetterISOLanguageName);
+
+                return regionalisedFileToUse;
+            }
+        }
+    }
+}
