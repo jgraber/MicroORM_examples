ef5589d77d46e1f11ad7ee2d70e81ffc3f4f0a47
Johnny Graber <jg@jgraber.ch>
2015-05-24 16:30:09 +0200

    Revert "different approach on DateTime validation"
    This reverts commit c6e65e59d29d33a0ca9e867a882e4da05feb282b.

--- MicroORM_Dapper/Quotations/App_Start/BundleConfig.cs
+++ MicroORM_Dapper/Quotations/App_Start/BundleConfig.cs
@@ -11,16 +11,8 @@ namespace Quotations
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/globalization")
-                .Include("~/Scripts/globalize.js")
-                .Include("~/Scripts/globalize.culture.*")
-                .Include("~/Scripts/globalize.initialize.js")
-                );
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
-                .Include("~/Scripts/jquery.validate*")
-                .Include("~/Scripts/globalize.validation.js")
-                );
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
