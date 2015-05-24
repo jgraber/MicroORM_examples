c6e65e59d29d33a0ca9e867a882e4da05feb282b
Johnny Graber <jg@jgraber.ch>
2015-05-24 16:30:06 +0200

    different approach on DateTime validation
    https://ajdotnet.wordpress.com/2014/01/25/asp-net-mvc-i18n-part-8-data-validation/

--- MicroORM_Dapper/Quotations/App_Start/BundleConfig.cs
+++ MicroORM_Dapper/Quotations/App_Start/BundleConfig.cs
@@ -11,8 +11,16 @@ namespace Quotations
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/globalization")
+                .Include("~/Scripts/globalize.js")
+                .Include("~/Scripts/globalize.culture.*")
+                .Include("~/Scripts/globalize.initialize.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include("~/Scripts/jquery.validate*")
+                .Include("~/Scripts/globalize.validation.js")
+                );
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
---
+++ MicroORM_Dapper/Quotations/Scripts/globalize.validation.js
@@ -0,0 +1,27 @@
+﻿// replace methods in jquery.validate.js ($.validator.methods) as necessary for localized validation:
+
+$.validator.methods.number = function (value, element) {
+    return this.optional(element) || !isNaN(Globalize.parseFloat(value));
+}
+
+$.validator.methods.date = function (value, element) {
+    if (this.optional(element))
+        return true;
+    var result = Globalize.parseDate(value);
+    return !isNaN(result) && (result != null);
+}
+
+$.validator.methods.min = function (value, element, param) {
+    return this.optional(element) || Globalize.parseFloat(value) >= param;
+}
+
+$.validator.methods.max = function (value, element, param) {
+    return this.optional(element) || Globalize.parseFloat(value) <= param;
+}
+
+$.validator.methods.range = function (value, element, param) {
+    if (this.optional(element))
+        return true;
+    var result = Globalize.parseFloat(value);
+    return (result >= param[0] && result <= param[1]);
+}
